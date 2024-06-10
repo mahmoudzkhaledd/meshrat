5ff8ffc70d585eaba7a92fe721a00ff6c8a4a8a4
Mahmoud Khaled <78917638+mahmoudzkhaledd@users.noreply.github.com>
2024-06-10 13:29:24 +0300

    cookies();

--- app/admin/(main)/layout.tsx
+++ app/admin/(main)/layout.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import AdminNavbar from '../_components/Navbar'
+import { cookies } from 'next/headers'
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
+    cookies();
     return (
         <div className="flex min-h-screen size-full w-full flex-col ">
             <AdminNavbar />
