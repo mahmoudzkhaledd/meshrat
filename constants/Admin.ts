f11bf11591fd3aeb095d7b84f2e479595a7a8173
mahmoudzkhaledd <mahmoudnaggar2002@gmail.com>
2024-06-24 17:16:29 +0300

    insighta

--- constants/Admin.ts
+++ constants/Admin.ts
@@ -1,35 +1,37 @@
 import { DollarSign, Home, LucideIcon } from "lucide-react";
 
 export interface NavbarProps {
-    name: string;
-    link: string;
-    newTap?: boolean;
-};
-
+  name: string;
+  link: string;
+  newTap?: boolean;
+}
 
 export const adminNavbarItems: NavbarProps[] = [
-    {
-        name: "Home",
-        link: "/admin",
-    },
+  {
+    name: "Home",
+    link: "/admin",
+  },
 
-    {
-        name: "Services",
-        link: "/admin/services",
-    },
-    {
-        name: "Blog",
-        link: "/admin/blogs",
-    },
-    {
-        name: "Info",
-        link: "/admin/info",
-    },
-    {
-        name: "Main Page",
-        link: "/",
-        newTap: true,
-    },
-
-
+  {
+    name: "Services",
+    link: "/admin/services",
+  },
+  {
+    name: "Blog",
+    link: "/admin/blogs",
+  },
+  {
+    name: "Info",
+    link: "/admin/info",
+  },
+  {
+    name: "Main Page",
+    link: "/",
+    newTap: true,
+  },
+  {
+    name: "Insights",
+    link: "https://insighta-snowy.vercel.app/websites/66797bf7753426f87e04e27c",
+    newTap: true,
+  },
 ];
