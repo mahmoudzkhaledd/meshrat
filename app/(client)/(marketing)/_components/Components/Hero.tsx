ccc370ad0a12e058be16d55a868bf172c879c763
mahmoudzkhaledd <mahmoudnaggar2002@gmail.com>
2024-06-26 21:26:24 +0300

    rtyrty

--- app/(client)/(marketing)/_components/Components/Hero.tsx
+++ app/(client)/(marketing)/_components/Components/Hero.tsx
@@ -13,46 +13,62 @@ import {
   MapPin,
   Siren,
 } from "lucide-react";
-type props = { icon: LucideIcon; wordKey: string };
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import Image from "next/image";
+type props = { icon: string; wordKey: string };
 const items: props[] = [
   {
     wordKey: "service",
-    icon: Clock,
+    icon: "/images/service.png",
+    // icon: Clock,
   },
   {
     wordKey: "price",
-    icon: CircleDollarSign,
+    icon: "/images/price.png",
+    // icon: CircleDollarSign,
   },
   {
     wordKey: "location",
-    icon: MapPin,
+    icon: "/images/map.png",
+    // icon: MapPin,
   },
   {
     wordKey: "expert",
-    icon: Siren,
+    icon: "/images/experience.png",
+    // icon: Siren,
   },
 ];
 
-const CustomCard = ({
-  icon: TopIcon,
-  text,
-}: {
-  icon: LucideIcon;
-  text: string;
-}) => {
+const CustomCard = ({ icon, text }: { icon: string; text: string }) => {
+  // return (
+  //   <div className="flex h-full w-full flex-col flex-wrap items-center justify-center gap-4 rounded-xl bg-muted px-5 py-5 text-center shadow">
+  //     <TopIcon />
+  //     <p className="text-lg font-semibold">{text}</p>
+  //   </div>
+  // );
   return (
-    <div className="w-xl flex flex-col items-center justify-center rounded-3xl bg-[#ffffffd1] p-6 shadow-xl md:px-4 lg:w-1/5">
-      <TopIcon size={35} className="mb-5" />
-      <h3 className="mb-2 text-xl font-bold">{text}</h3>
-    </div>
+    <Card className="flex h-[250px] md:h-[200px] w-full flex-col items-center justify-center overflow-hidden shadow-lg transition-all duration-500 hover:border-2 hover:border-blue-600">
+      <CardHeader>
+        <Image src={icon} alt="logo" width={60} height={60} />
+      </CardHeader>
+      <CardContent className="text-balance text-center text-xl font-bold">
+        {text}
+      </CardContent>
+    </Card>
   );
+  // return (
+  //   <div className="w-xl flex flex-col items-center justify-center rounded-3xl bg-[#ffffffd1] p-6 shadow-xl md:px-4 lg:w-1/5">
+  //     <TopIcon size={35} className="mb-5" />
+  //     <h3 className="mb-2 text-xl font-bold">{text}</h3>
+  //   </div>
+  // );
 };
 
 export default function HeroSection() {
   const t = useTranslations("homePage.hero");
   const lang = getLanguage();
   return (
-    <section className="pt-24 md:pt-32" id="home">
+    <section className="pt-24 md:pt-20" id="home">
       <div className="flex flex-col-reverse items-center justify-between gap-10 text-center md:flex-row md:text-left">
         <div
           className={cn("tracking-wider md:tracking-normal", {
@@ -78,7 +94,7 @@ export default function HeroSection() {
           />
         </div>
       </div>
-      <div className="xs:px-16 mt-10 flex flex-wrap items-center justify-center gap-5 px-10 text-center sm:px-5 md:flex-nowrap md:justify-around md:px-0">
+      <div className="mt-20 flex flex-wrap items-center justify-center gap-5 px-10 text-center sm:px-5 md:flex-nowrap md:justify-around md:px-0">
         {items.map((e, idx) => (
           <CustomCard key={idx} icon={e.icon} text={t(e.wordKey)} />
         ))}
