a4060516f86f0b2f606978ada592cfcc5c75ecde
mahmoudzkhaledd <mahmoudnaggar2002@gmail.com>
2024-06-27 19:34:22 +0300

    adasdasd

--- app/(client)/(marketing)/_components/Components/ServicesSection.tsx
+++ app/(client)/(marketing)/_components/Components/ServicesSection.tsx
@@ -12,7 +12,7 @@ export default function ServicesSection({ services }: { services: Service[] }) {
 
   return (
     <section className="pt-24 md:pt-32" id="services">
-      <h2 className="mb-10 text-center text-4xl font-bold">{t('title')}</h2>
+      <h2 className="mb-10 text-center text-4xl font-bold">{t('title')}</h2> 
       <div className="flex flex-col gap-4 lg:grid lg:grid-cols-3">
         {services?.map((e, idx) => <ServiceCard service={e} key={idx} />)}
       </div>
